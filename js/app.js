9fdd5588c3b77183264f9267b866faa72d0d77ee
Ashley Davison <ashley.e.davison@gmail.com>
2015-10-10 15:38:05 -0400

    Render player and enemies

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-/*// Enemies our player must avoid
-var Enemy = function() {
+// Enemies our player must avoid
+var Enemy = function(x, y) { //I added this.x and this.y
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    // var obj = {};
+    // obj.move = function() {
+    //   obj.loc++;
+    // };
+    // return obj; //I added lines 5-9 so far they do nothing
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x; //
+    this.y = y;//
+    this.speed = 1;//
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +21,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    //math.random function?
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +33,33 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+  //add player class info
+  this.x = x;
+  this.y = y; //i added does nothing
+  this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function() {
+    //add the code here does nothing
+};
 
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y); //i added does nothing
+};
+
+Player.prototype.handleInput = function () {
+  //is this right? add the rest of the code. does nothing currently
+};
+ var player = new Player(200, 400); //i added does nothing
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [
+  new Enemy(000, 60),
+  new Enemy(000, 140),
+  new Enemy(000, 230) //I added this it does nothing
+];
 
 
 // This listens for key presses and sends the keys to your
@@ -44,4 +74,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-*/
--- js/engine.js
+++ js/engine.js
@@ -78,10 +78,10 @@ var Engine = (function(global) {
      * functionality this way (you could just implement collision detection
      * on the entities themselves within your app.js file).
      */
-//    function update(dt) {
-//        updateEntities(dt);
+    function update(dt) {
+        updateEntities(dt);
         // checkCollisions();
-//    }
+    }
 
     /* This is called by the update function  and loops through all of the
      * objects within your allEnemies array as defined in app.js and calls
@@ -90,12 +90,12 @@ var Engine = (function(global) {
      * the data/properties related to  the object. Do your drawing in your
      * render methods.
      */
-  //  function updateEntities(dt) {
-  //      allEnemies.forEach(function(enemy) {
-  //          enemy.update(dt);
-  //      });
-  //      player.update();
-//    }
+    function updateEntities(dt) {
+        allEnemies.forEach(function(enemy) {
+            enemy.update(dt);
+        });
+        player.update();
+    }
 
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
